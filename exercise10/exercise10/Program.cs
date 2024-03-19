ad9107e5943247fdd2177282533e201614ef3e47
WaveextOnXX <145377312+WaveextOnXX@users.noreply.github.com>
2024-03-19 11:36:14 -0400

    finished 1 AND 2

--- exercise10/exercise10/Program.cs
+++ exercise10/exercise10/Program.cs
@@ -10,31 +10,84 @@
             Console.WriteLine("3 - settings ");
             Console.WriteLine("4 - quit game ");
             Console.WriteLine("***************");
-            
+
             int response = int.Parse(Console.ReadLine());
-            if (response == 1) 
+            if (response == 1)
             {
                 Console.WriteLine("loading new game");
             }
 
-            if (response == 2) 
+            if (response == 2)
             {
                 Console.WriteLine("loading files");
             }
 
-            if (response == 3) 
+            if (response == 3)
             {
                 Console.WriteLine("brightness");
                 Console.WriteLine("volume");
                 Console.WriteLine("graphics");
             }
 
-            if (response == 4) 
+            if (response == 4)
             {
-                Console.WriteLine("quiting game");
+                Console.WriteLine("going back to dashboard");
             }
-            
+            if (response < 0)
+            {
+                Console.WriteLine("invalid input");
+            }
+
+            if (response > 4)
+            {
+                Console.WriteLine("invalid input");
+            }
+
+            while (response != 4) ;
+            {
+                Console.WriteLine("**************");
+                Console.WriteLine("1 - New game");
+                Console.WriteLine("2 - load game ");
+                Console.WriteLine("3 - settings ");
+                Console.WriteLine("4 - quit game ");
+                Console.WriteLine("***************");
+            }
+
+           
+            if (response == 1)
+            {
+                Console.WriteLine("loading new game");
+            }
+
+            if (response == 2)
+            {
+                Console.WriteLine("loading files");
+            }
+
+            if (response == 3)
+            {
+                Console.WriteLine("brightness");
+                Console.WriteLine("volume");
+                Console.WriteLine("graphics");
+            }
+
+            if (response == 4)
+            {
+                Console.WriteLine("going back to dashboard");
+            }
+
+            if (response < 0)
+            {
+                Console.WriteLine("invalid input");
+            }
+
+            if (response > 4)
+            {
+                Console.WriteLine("invalid input");
+
+            }
 
         }
-    }
+
+    }   
 }
